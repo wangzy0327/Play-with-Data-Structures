2e09e68ce29f1026825285798c26b1de757b1dd6
wangzy0327 <wangzy0327@qq.com>
2021-05-08 17:00:21 +0800

    add non-recursion pre-order,in-order,post-order implements

---
+++ 06-Binary-Search-Tree/09-Non-Recursion-Preorder-Traverse-in-BST/src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+        BST<Integer> bst = new BST<>();
+        int[] nums = {5, 3, 6, 8, 4, 2};
+        for(int num:nums)
+            bst.add(num);
+
+        ///////////////////
+        //         5     //
+        //        / \    //
+        //       3  6    //
+        //      / \  \   //
+        //     2  4   8  //
+        ////////////////////
+//        bst.preOrder();
+        // 5 3 2 4 6 8
+//        System.out.println();
+//        System.out.println(bst);
+//        bst.preOrderNR();
+//        System.out.println();
+
+//        bst.inOrder();
+        //2 3 4 5 6 8
+//        System.out.println();
+//        bst.inOrderNR();
+//        System.out.println();
+
+        bst.postOrder();
+        //2 4 3 8 6 5
+        System.out.println();
+//        bst.postOrderNR();
+        bst.postOrderNR2();
+        System.out.println();
+    }
+}
